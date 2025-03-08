0ccc92e9de4d498e4308fd90fd898328289605f9
Aashna Sharma <158739704+aashna306@users.noreply.github.com>
2025-03-08 14:10:06 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const chatRoutes = require('./routes/chatRoute');
-//const translateRoute = require("./routes/translate");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
