929a1e66f9819faf25eeb1cea9fbdc6f599086ac
arpita-goel <148207309+arpita-goel@users.noreply.github.com>
2025-03-08 23:10:38 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,9 +5,11 @@ const chatRoutes = require('./routes/chatRoute');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
+
 
 app.use('/api', chatRoutes);
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT,'0.0.0.0', () => console.log(`Server running on http://localhost:${PORT}`));
+
