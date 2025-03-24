4a1b5deb221e2fca6c153cf185c3d342120b600d
arpita-goel <148207309+arpita-goel@users.noreply.github.com>
2025-03-24 23:09:04 +0530

    Update OTP_page.dart

--- lib/pages/OTP_page.dart
+++ lib/pages/OTP_page.dart
@@ -1,15 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gsc_project/colors/app_colors.dart';
-import 'package:gsc_project/main.dart';
 import 'package:gsc_project/pages/home_page.dart';
 
-void main() {
-  runApp(const MyApp());
+class OtpPage extends StatefulWidget {
+  const OtpPage(
+      {super.key, required this.phonenumber, required this.verificationId});
+  final String phonenumber;
+  final String verificationId;
+
+  @override
+  State<OtpPage> createState() => _OtpPageState();
 }
 
-class OtpPage extends StatelessWidget {
-  const OtpPage({super.key});
+class _OtpPageState extends State<OtpPage> {
+  final List<TextEditingController> otpControllers =
+      List.generate(6, (index) => TextEditingController());
+  final FocusNode focusNodes = FocusNode();
+  
+  String getOtpCode() {
+    return otpControllers.map((controller) => controller.text).join();
+  }
 
+  Future<void> verifyOtp() async {
+    String otp = getOtpCode();
+
+    if (otp.length != 6) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Please enter a valid 6-digit OTP")),
+      );
+      return;
+    }
+
+    try {
+      final credential = PhoneAuthProvider.credential(
+          verificationId: widget.verificationId, smsCode: otp);
+
+      await FirebaseAuth.instance.signInWithCredential(credential);
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("OTP Verification Failed: ${e.toString()}")),
+      );
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,17 +79,36 @@ class OtpPage extends StatelessWidget {
                   return SizedBox(
                     width: 40,
                     child: TextFormField(
+                      controller: otpControllers[index],
+                      focusNode: focusNodes,
                       textAlign: TextAlign.center,
                       decoration: InputDecoration(
+                            counterText: "",
                         enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: AppColors.lineColor, width: 3.0),
+                          borderSide:
+                              BorderSide(color: AppColors.lineColor, width: 3.0),
                         ),
                         focusedBorder: UnderlineInputBorder(
                           borderSide: BorderSide(color: AppColors.pink, width: 3.0),
                         ),
+                        // enabledBorder: UnderlineInputBorder(
+                        //   borderSide: BorderSide(
+                        //       color: AppColors.lineColor, width: 3.0),
+                        // ),
+                        // focusedBorder: UnderlineInputBorder(
+                        //   borderSide:
+                        //       BorderSide(color: AppColors.pink, width: 3.0),
+                        // ),
                       ),
-                      //keyboardType: TextInputType.number,
-                      //maxLength: 1,
+                      keyboardType: TextInputType.number,
+                      maxLength: 1,
+                        onChanged: (value) {
+                        if (value.isNotEmpty && index < 5) {
+                          FocusScope.of(context).nextFocus();
+                        } else if (value.isEmpty && index > 0) {
+                          FocusScope.of(context).previousFocus();
+                        }
+                      },
                     ),
                   );
                 }),
@@ -74,27 +128,22 @@ class OtpPage extends StatelessWidget {
             ),
             const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => HomePage()),
-                );
-              },
+              onPressed: verifyOtp ,
               style: ElevatedButton.styleFrom(
                 backgroundColor: AppColors.ButtonColor,
-                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
                 textStyle: const TextStyle(
                   fontSize: 20,
                 ),
               ),
               child: const Text(
-                  "Verify",
-                  style: TextStyle(
+                "Verify",
+                style: TextStyle(
                     fontSize: 18,
                     color: Colors.black,
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
+                    fontWeight: FontWeight.bold),
+              ),
             ),
           ],
         ),
