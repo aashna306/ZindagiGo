220a22880cb999e54c2ed9e40dfb08cc7eea7144
Aashna Sharma <158739704+aashna306@users.noreply.github.com>
2025-03-25 00:33:57 +0530

    Added medical record

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,16 +6,21 @@ const bodyParser = require("body-parser");
 const admin = require("firebase-admin");
 const userRoutes = require("./routes/userRoute");
 const chatRoutes = require('./routes/chatRoute');
+const medicalRecordRoutes = require('./routes/medicalRecordRoute');
 //const reminderRoutes = require('./routes/reminder');
+//const exerciseRoutes = require('./routes/exerciseRoute');
 const app = express();
-
 
 const serviceAccount = require("./config/serviceAccountKey.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-mongoose.connect(process.env.MONGO_URI);
+MONGO_URI=process.env.MONGO_URI
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use(cors({ origin: '*' }));
 app.use(bodyParser.json());
@@ -24,5 +29,12 @@ app.use(express.json());
 app.use('/api', chatRoutes);
 //app.use('/api', reminderRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/medical-records", medicalRecordRoutes);
+//app.use('/api/exercises', exerciseRoutes);
 
-module.exports = app;
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
