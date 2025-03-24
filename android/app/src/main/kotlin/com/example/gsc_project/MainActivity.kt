7ed40f60e73f266cc270e58360a597c4813bc9e6
arpita-goel <148207309+arpita-goel@users.noreply.github.com>
2025-03-24 23:16:37 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/gsc_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/gsc_project/MainActivity.kt
@@ -1,5 +1,36 @@
-package com.example.gsc_project
+package com.example.gsc_project 
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example.gsc_project/call" 
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "makeCall") {
+                val phoneNumber = call.argument<String>("phoneNumber")
+                if (phoneNumber != null) {
+                    makePhoneCall(phoneNumber)
+                    result.success("Call started")
+                } else {
+                    result.error("INVALID_NUMBER", "Phone number is null", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun makePhoneCall(phoneNumber: String) {
+        val intent = Intent(Intent.ACTION_CALL)
+        intent.data = Uri.parse("tel:$phoneNumber")
+        startActivity(intent)
+    }
+}
