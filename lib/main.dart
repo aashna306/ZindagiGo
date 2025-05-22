0524e529abc4aa4b0788ddfec559c3d98a52e9e6
arpita-goel <148207309+arpita-goel@users.noreply.github.com>
2025-05-22 14:57:47 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:gsc_project/pages/MedicalRecords.dart';
+import 'package:gsc_project/pages/notifications.dart';
 import 'firebase_options.dart';
 import 'package:gsc_project/pages/new_Account.dart';
 import 'package:gsc_project/pages/home_page.dart';
@@ -11,6 +12,7 @@ import 'dart:async';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,8 +26,8 @@ void main() async {
   if (!isRunning) {
     service.startService();
   }
-
-  runApp(const MyApp());
+  await NotificationService.initialize(); 
+  runApp( MyApp());
 }
 
 Future<void> checkAndRequestBatteryOptimization() async {
@@ -80,15 +82,6 @@ Future<void> initializeBackgroundService() async {
 
 // Background service function
 FutureOr<bool> _onBackgroundServiceStart(ServiceInstance service) async {
-  // if (service is AndroidServiceInstance) {
-  //   service.setAsForegroundService();
-
-  //   service.setForegroundNotificationInfo(
-  //     title: "Fall Detection Active",
-  //     content: "Monitoring falls...",
-  //   );
-  // }
-
  if (service is AndroidServiceInstance) {
     service.setForegroundNotificationInfo(
       title: "Zindagi Go - Fall Detection Active",
