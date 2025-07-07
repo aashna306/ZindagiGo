919551bae4a18961ca3454d84fddc93e49868c9c
Aashna Sharma <158739704+aashna306@users.noreply.github.com>
2025-07-07 17:20:32 +0530

    Update userModel.js

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -8,6 +8,11 @@ const userSchema = new mongoose.Schema({
   phone: String,
   email: String,
   emergencyContact: String,
+  fcmToken: String,
+   role: { type: String, enum: ['elderly', 'caretaker'], default: 'elderly' },
+  hasCaretaker: { type: Boolean, default: false },
+  caretakerEmail: String,
+  caretakerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 });
 const User = mongoose.model("User", userSchema, "users");
 module.exports = User;
